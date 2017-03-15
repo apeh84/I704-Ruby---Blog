d8b5343e2368ba2518afea99ad24d4607c0b70fe
Gabriel Apeh Adoyi <gabriel.adoyi@itcollege.ee>
2017-03-15 16:28:06 +0200

    Create form for posts and helper function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,22 @@
 class ArticlesController < ApplicationController
-def index
+	include ArticlesHelper
+	def index
+	
+	
+
+    def create
+		@article = Article.new(article_params)
+        @article.save
+
+        redirect_to article_path(@article)
+    end
+
 	def show
 		@article = Article.find(params[:id])
-	
 	end
-  @articles = Article.all
-end
+	    @articles = Article.all
+	def new
+	    @article = Article.new
+	end
+	end
 end
